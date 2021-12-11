27d7f2a09fcf5ac5941ae2a843204de165e651c4
JiquanWang99 <951873902@qq.com>
2021-12-11 13:47:31 +0800

    feat: add the env config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import * as env from 'dotenv';
+
+// 从根目录 .env 文件获取配置
+env.config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
---
+++ src/types/env.d.ts
@@ -0,0 +1,6 @@
+// 定义.env 文件中的数据类型
+namespace NodeJS {
+  interface ProcessEnv {
+    [key: string]: any;
+  }
+}
