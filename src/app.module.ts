4a11eed964d335d6181465e194ca4c935f0c79f1
JiquanWang99 <951873902@qq.com>
2021-12-12 22:19:35 +0800

    feat: add mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ApiModule } from './module/client/api.module';
+import { AdminModule } from './module/admin/admin.module';
 
 @Module({
-  imports: [ApiModule],
-  controllers: [],
   providers: [],
+  imports: [
+    ApiModule,
+    AdminModule,
+    MongooseModule.forRootAsync({
+      useFactory: () => ({ uri: process.env.MONGO_URL }),
+    }),
+  ],
+  controllers: [],
 })
 export class AppModule {}
---
+++ src/db/schema/user/user.schema.ts
@@ -0,0 +1,34 @@
+import * as dayjs from 'dayjs';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { v4 as uuidv4 } from 'uuid';
+import { hashSync } from 'bcryptjs';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  username: string;
+
+  @Prop({
+    set: hashSync,
+    required: true,
+  })
+  password: string;
+
+  @Prop()
+  email?: string;
+
+  @Prop()
+  phone?: string;
+
+  @Prop({
+    default: +new Date(),
+    get: (val: string) => dayjs(val).format('YYYY-DD-MM HH:mm'),
+  })
+  create_time?: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
